34097d5853cacd5321793be612738ed958514dd8
Stackwyre <stackwyre@gmail.com>
2026-05-02 20:44:06 -0500

    Address review feedback: update src/bounty/dto/claim-bounty.dto.ts

--- src/bounty/dto/claim-bounty.dto.ts
+++ src/bounty/dto/claim-bounty.dto.ts
@@ -1,7 +1,7 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ClaimBountyDto {
-  @IsString()
   @IsNotEmpty()
-  claimantId: string;
+  @IsString()
+  claimedBy: string;
 }
