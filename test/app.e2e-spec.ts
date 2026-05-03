9d79c6f603031013e8cb9749652567fbe061de3d
Stackwyre <stackwyre@gmail.com>
2026-05-03 18:15:29 -0500

    Address review feedback: update test/app.e2e-spec.ts

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
@@ -12,13 +12,6 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-      }),
-    );
     await app.init();
   });
 
@@ -27,38 +20,5 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
-  });
-
-  describe('/bounties', () => {
-    it('should create a bounty', () => {
-      const createBountyDto = {
-        title: 'Test Bounty',
-        description: 'This is a test bounty description',
-        amount: 100,
-        creatorId: 'user123',
-      };
-
-      return request(app.getHttpServer())
-        .post('/bounties')
-        .send(createBountyDto)
-        .expect(201)
-        .expect((res) => {
-          expect(res.body.title).toBe(createBountyDto.title);
-          expect(res.body.status).toBe('open');
-        });
-    });
-
-    it('should get all bounties', () => {
-      return request(app.getHttpServer())
-        .get('/bounties')
-        .expect(200)
-        .expect((res) => {
-          expect(Array.isArray(res.body)).toBe(true);
-        });
-    });
-  });
-
-  afterAll(async () => {
-    await app.close();
   });
 });
