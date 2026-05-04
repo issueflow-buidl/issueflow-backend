fb1dce0440520cbd4885fe5a0e7a29c4c596a93e
Stackwyre <stackwyre@gmail.com>
2026-05-04 09:25:52 -0500

    Address review feedback: update src/bounty/dto/create-bounty.dto.ts

--- src/bounty/dto/create-bounty.dto.ts
+++ src/bounty/dto/create-bounty.dto.ts
@@ -11,9 +11,9 @@ export class CreateBountyDto {
 
   @IsNumber()
   @IsPositive()
-  reward: number;
+  amount: number;
 
   @IsString()
   @IsNotEmpty()
-  creatorId: string;
+  createdBy: string;
 }
