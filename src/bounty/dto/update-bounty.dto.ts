a60a1144aff8cec8fbb6473df853cb2e9dfeaec3
Stackwyre <stackwyre@gmail.com>
2026-05-05 14:44:00 -0500

    Address review feedback: update src/bounty/dto/update-bounty.dto.ts

--- src/bounty/dto/update-bounty.dto.ts
+++ src/bounty/dto/update-bounty.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBountyDto } from './create-bounty.dto';
+import { IsOptional, IsEnum } from 'class-validator';
+import { BountyStatus } from '../bounty.entity';
 
-export class UpdateBountyDto extends PartialType(CreateBountyDto) {}
+export class UpdateBountyDto extends PartialType(CreateBountyDto) {
+  @IsOptional()
+  @IsEnum(BountyStatus)
+  status?: BountyStatus;
+}
