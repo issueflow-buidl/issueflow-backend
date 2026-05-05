b87a4e9731a3e5450c9bdb1dccb95c24a0c1c805
Stackwyre <stackwyre@gmail.com>
2026-05-05 17:15:56 -0500

    Address review feedback: update src/bounty/dto/create-bounty.dto.ts

--- src/bounty/dto/create-bounty.dto.ts
+++ src/bounty/dto/create-bounty.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
+import { IsString, IsNumber, IsNotEmpty, IsPositive } from 'class-validator';
 
 export class CreateBountyDto {
   @IsString()
@@ -15,5 +15,5 @@ export class CreateBountyDto {
 
   @IsString()
   @IsNotEmpty()
-  creatorId: string;
+  createdBy: string;
 }
