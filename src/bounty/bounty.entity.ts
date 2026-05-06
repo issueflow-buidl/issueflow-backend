9a7d486ce0f3fe72167df3bb7c13ffbcd5b64ec7
Stackwyre <stackwyre@gmail.com>
2026-05-05 19:16:18 -0500

    Address review feedback: update src/bounty/bounty.entity.ts

--- src/bounty/bounty.entity.ts
+++ src/bounty/bounty.entity.ts
@@ -34,21 +34,15 @@ export class Bounty {
   })
   status: BountyStatus;
 
-  @Column({ name: 'created_by' })
+  @Column({ nullable: true })
   createdBy: string;
 
-  @Column({ name: 'claimed_by', nullable: true })
-  claimedBy?: string;
+  @Column({ nullable: true })
+  claimedBy: string;
 
-  @CreateDateColumn({ name: 'created_at' })
+  @CreateDateColumn()
   createdAt: Date;
 
-  @UpdateDateColumn({ name: 'updated_at' })
+  @UpdateDateColumn()
   updatedAt: Date;
-
-  @Column({ name: 'claimed_at', nullable: true })
-  claimedAt?: Date;
-
-  @Column({ name: 'completed_at', nullable: true })
-  completedAt?: Date;
 }
