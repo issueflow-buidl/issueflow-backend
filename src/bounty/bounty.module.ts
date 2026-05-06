9654b78f1bd5da47fff70c6fbc327dbdafd4b06a
Stackwyre <stackwyre@gmail.com>
2026-05-06 17:51:55 -0500

    Address review feedback: update src/bounty/bounty.module.ts

--- src/bounty/bounty.module.ts
+++ src/bounty/bounty.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { BountyService } from './bounty.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BountyController } from './bounty.controller';
+import { BountyService } from './bounty.service';
+import { Bounty } from './bounty.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Bounty])],
   controllers: [BountyController],
   providers: [BountyService],
+  exports: [BountyService],
 })
 export class BountyModule {}
